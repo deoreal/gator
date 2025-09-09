0cd5149a3bf2b0b20d956c93904fca85d4c5ac5f
deoreal <milisav@radmanic.de>
2025-09-09 22:29:19 +0200

    added more func - incomplete

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,44 +8,58 @@ import (
 	"os"
 )
 
-const configFileName = "/Users/milisav/.gatorconfig.json"
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func ReadConfig() (Config, error) {
-	file, err := os.Open(configFileName)
+func (cfg *Config) SetUser(user string) {
+	cfg.CurrentUserName = user
+}
+
+func ReadConfig() (*Config, error) {
+	homedir, err := os.UserHomeDir()
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to read config file: %s", configFileName)
+		return &Config{}, fmt.Errorf("failed to read HomeDir: %s", err)
+	}
+	file, err := os.Open(homedir + "/" + configFileName)
+	if err != nil {
+		return &Config{}, fmt.Errorf("failed to read config file: %s", configFileName)
 	}
 	defer file.Close()
 
 	data, err := io.ReadAll(file)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to read data from config file: %s", configFileName)
+		return &Config{}, fmt.Errorf("failed to read data from config file: %s", configFileName)
 	}
 
 	cfg := Config{}
 
 	err = json.Unmarshal(data, &cfg)
 	if err != nil {
-		return Config{}, fmt.Errorf("failed to read config into json object")
+		return &Config{}, fmt.Errorf("failed to read config into json object")
 	}
 
-	return cfg, nil
+	return &cfg, nil
 }
 
-func WriteConfig(cfg *Config) error {
+func WriteConfig(cfg Config) error {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("failed to read HomeDir: %s", err)
+	}
+
 	data, err := json.Marshal(cfg)
 	if err != nil {
 		return fmt.Errorf("failed to marshal data into json object")
 	}
 
-	err = os.WriteFile(configFileName, data, 0o644)
+	err = os.WriteFile(homedir+"/"+configFileName, data, 0o644)
 	if err != nil {
 		return fmt.Errorf("failed to write config file: %s - %s", configFileName, err)
 	}
 	return nil
 }
+
--- main.go
+++ main.go
@@ -2,25 +2,92 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/deoreal/gator/internal/config"
 )
 
+const configFileName = ".gatorconfig.json"
+
+type state struct {
+	conf *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	cmds map[string]func(*state, command) error
+}
+
+func getConfigFilePath() (string, error) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to read homedir: %s", err)
+	}
+	return homedir + "/" + configFileName, nil
+}
+
+func handlerLogins(st *state, cmd command) error {
+	if cmd.args == nil {
+		return fmt.Errorf("command is missing")
+	}
+
+	switch cmd.name {
+	case "login":
+		cmd.args[0] = st.conf.CurrentUserName
+		fmt.Println("user has been set to: ", cmd.args[0])
+	}
+
+	return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	return nil
+}
+
+func (c *commands) register(name string, f func(s *state, cmd command) error) {
+	c.cmds[name] = f
+}
+
+func login(s *state, cmd command) error {
+	return nil
+}
+
 func main() {
-	var cfg config.Config
-	fmt.Println("This is blogbot")
+	c := &commands{}
+	s := &state{}
+	var err error
 
-	cfg.DBURL = "postgres://newDB"
-	cfg.CurrentUserName = "mili"
+	s.conf, err = config.ReadConfig()
+	if err != nil {
+		fmt.Printf("failed to read config: %s\n", err)
+	}
 
-	if err := config.WriteConfig(&cfg); err != nil {
-		log.Fatalf("failed to write config: %s", err)
+	c.register("login", login)
+
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments")
 		os.Exit(1)
 	}
 
-	cfg, _ = config.ReadConfig()
+	kommando := command{name: os.Args[0], args: os.Args[1:]}
 
-	fmt.Printf("cfg: %v", cfg)
+	/*
+	   err = config.WriteConfig(*cfg)
+
+	   	if err != nil {
+	   		fmt.Printf("failed to write config: %s", err)
+	   	}
+
+	   cfg, err = config.ReadConfig()
+
+	   	if err != nil {
+	   		fmt.Printf("failed to read config: %s\n", err)
+	   	}
+
+	   fmt.Println(cfg.DBURL, cfg.CurrentUserName)
+	*/
 }
